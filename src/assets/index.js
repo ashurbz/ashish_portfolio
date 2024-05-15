01be1bae39edb835ef7aee77cf6bded9948d465c
Ashish <rbzashu1@gmail.com>
2024-05-15 20:35:47 +0530

    added experince and about

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,6 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -20,15 +19,15 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
+import addicor from "./company/addicor.png";
+import monocept from "./company/monocept.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import ps from "./company/ps.svg";
+import logrelic from "./company/logrelic.jpg";
+import netflix from "./Netflix.png";
+import worldwise from "./worldwise.png";
+import youtube from "./youtube.png";
 
 export {
   logo,
@@ -52,11 +51,13 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
+  netflix,
   jobit,
   tripguide,
+  addicor,
+  monocept,
+  ps,
+  logrelic,
+  worldwise,
+  youtube,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -10,19 +10,17 @@ import {
   reactjs,
   redux,
   tailwind,
-  nodejs,
   mongodb,
   git,
   figma,
   docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
-  carrent,
-  jobit,
-  tripguide,
-  threejs,
+  monocept,
+  addicor,
+  ps,
+  logrelic,
+  netflix,
+  worldwise,
+  youtube,
 } from "../assets";
 
 export const navLinks = [
@@ -89,16 +87,12 @@ const technologies = [
     icon: tailwind,
   },
   {
-    name: "Node JS",
-    icon: nodejs,
+    name: "Next JS",
+    icon: creator,
   },
   {
     name: "MongoDB",
     icon: mongodb,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
   },
   {
     name: "git",
@@ -116,55 +110,53 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
+    title: "Software Engineer",
+    company_name: "Addicor Technologies Pvt. Ltd.",
+    icon: addicor,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "Nov 2019 - March 2021",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Collaborated with back-end developers to integrate RESTful APIs, ensuring efficient data flow and functionality.",
+      "Utilized Redux for state management, improving application scalability and maintainability,reducing state-related bugs by 40%.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Conducted performance optimization,reducing load times by 30% through code splitting and lazy loading techniques.",
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
+    title: "Software Engineer",
+    company_name: "Monocept Consulting Pvt. Ltd.",
+    icon: monocept,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "March 2021 - Jan 2022",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Developed and maintained a web application with over 100,000 active users using React.js,resulting in a 30% increase in user engagement.",
+      "Took part in the team’s agile software development process, developed unit tests and functional/integration tests, supported production deployments of developed solutions",
+      "Developed front-end end-to-end where customers can buy insurance without help of any agent",
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
+    title: "Associate Technology (FrontEnd)",
+    company_name: "Publicis Sapient",
+    icon: ps,
     iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    date: "Jan 2022 - April 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Assisted in determining operational feasibility through analysis, problem definition,requirement analysis, and proposing solutions",
+      "Mentored a team of 3 junior developers on React best practices, leading to a 15% improvement in team productivity.",
+      "Managed the maintenance of program modules including operational support, problem resolution, preventative and corrective maintenance and enhancements",
+      "Documented software solutions through flowcharts, layouts, diagrams, charts, code comments and clear code",
     ],
   },
   {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
+    title: "Freelancer FrontEnd Developer",
+    company_name: "LogRelic",
+    icon: logrelic,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "Sep 2023 - Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "working on Next JS, Tailwind Css, TypeScript",
     ],
   },
 ];
@@ -172,84 +164,42 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "I had the pleasure of working alongside Ashish in the same team during our time at our previous company, and I can confidently say that he was an invaluable asset to our team. Ashish consistently impressed with his strong work ethic, dedication, and attention to detail.",
+    name: "Ankur Kumar Rai",
+    designation: "Associate Solution Leader",
+    company: "Brane Enterprises Pvt. Ltd.",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
+      "Ashish is unmatched at React skill. He is ordered, meticulous, and a swift learner. All these The a fore mentioned qualities were significant when trying to implement project requirements . Being a quick at learning let him comprehend the market fit and wisely choose the optimum path to success",
+    name: "Robin Kumar Gupta",
+    designation: "Deputy Manager",
+    company: "Jio",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+      "I have worked with Ashish for over 1 year. I was impressed by his React JS and JavaScript. Anyone would be lucky to have Ashish as a colleague. He is always available to help us when needed, He strives for excellence and is a valuable team player",
+    name: "Gaurav Garg",
+    designation: "Associate Technology L2",
+    company: "Publicis Sapient",
+    image: "https://randomuser.me/api/portraits/men/6.jpg",
   },
 ];
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Netflix Clone",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web-based platform in which we can see trailers of webseries from netflix",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: carrent,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Job IT",
-    description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
+        name: "JavaScript",
         color: "green-text-gradient",
       },
       {
@@ -257,8 +207,50 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
+    image: netflix,
+    source_code_link: "https://github.com/ashurbz/netflix-clone-react-app",
+  },
+  {
+    name: "WorldWise",
+    description:
+      "Web App to keep track of all cities or countries we have travelled and add notes about them. We can get location by clicking on the map.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "module css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "context",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: worldwise,
+    source_code_link: "https://github.com/ashurbz/worldwise",
+  },
+  {
+    name: "Youtube Clone",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind CSS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: youtube,
+    source_code_link: "https://github.com/ashurbz/Youtube",
   },
 ];
 
